8d9f095861179db9a2906263f8b4723c1ecdcd71
chinemerem2525 <chubexchange@gmail.com>
2024-03-16 08:59:30 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { JobComponent } from './job/job.component';
+import { ContactComponent } from './shared/contact/contact.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { JobDetailComponent } from './shared/job-detail/job-detail.component';
+import { ApplicationComponent } from './application/application.component';
+import { BlogComponent } from './shared/blog/blog.component';
+import { BlogDetailsComponent } from './shared/blog-details/blog-details.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HomeComponent, JobComponent, ContactComponent, HeaderComponent, FooterComponent, JobDetailComponent, ApplicationComponent, BlogComponent, BlogDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { JobComponent } from './job/job.component';
+import { ApplicationComponent } from './application/application.component';
+import { SharedComponent } from './shared/shared.component';
+import { ContactComponent } from './shared/contact/contact.component';
+import { JobDetailComponent } from './shared/job-detail/job-detail.component';
+import { BlogComponent } from './shared/blog/blog.component';
+import { BlogDetailsComponent } from './shared/blog-details/blog-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {'path': '', component:HomeComponent},
+  {'path': 'job', component:JobComponent},
+  {'path': 'app', component:ApplicationComponent},
+  {'path': 'shared', component:SharedComponent},
+  {'path': 'contact', component:ContactComponent},
+  {'path': 'job-detail', component:JobDetailComponent},
+  {'path': 'blog', component:BlogComponent},
+  {'path': 'blog-details', component:BlogDetailsComponent},
+];
